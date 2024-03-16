1bd8f2c182977ed322855604a8007735941147c0
Max <madmax3131@yandex.ru>
2024-03-16 14:35:08 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const penisLength = +prompt('How long is your penis?', '15');
+penisLength < 15 ? alert('Hi,короткоствол!') : alert ('Hi,пиздабол!');
+
+console.log(4 + +'5');
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+
+decr--;
